4ec7b6e413bbd0cb9e45d679d707995f9662f9fa
elibinary <binaryeli@gmail.com>
2019-04-20 21:11:50 +0800

    add Initialize method to load settings

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/clippingkk/common
 go 1.12
 
 require (
+	github.com/jinzhu/copier v0.0.0-20180308034124-7e38e58719c3
 	github.com/spf13/afero v1.2.2
 	github.com/stretchr/testify v1.3.0
 )
--- settings/settings_test.go
+++ settings/settings_test.go
@@ -1,7 +1,32 @@
 package settings
 
 func (suitor *Suitor) TestFetchFile()  {
-	file, err := suitor.loader.fetchFile()
+	err := suitor.loader.fetchFile()
 	suitor.NoError(err)
-	suitor.Equal("test.dev.json", file)
+	suitor.Equal("test.dev.json", suitor.loader.file)
+}
+
+func (suitor *Suitor)TestLoadSettings()  {
+	var cfg *AppConfig
+
+	suitor.loader.container = &AppConfig{}
+	err := suitor.loader.loadSettings()
+	suitor.NoError(err)
+
+	cfg, ok := suitor.loader.container.(*AppConfig)
+	suitor.True(ok)
+	suitor.Equal("test-app", cfg.AppName)
+	suitor.Equal(true, cfg.Debug)
+	suitor.Equal("test", cfg.AppEnv)
+	suitor.Equal("root@tcp(localhost:3306)/test", cfg.Database.DSN)
+}
+
+func (suitor *Suitor)TestInitialize() {
+	cfg := &AppConfig{}
+	err := Initialize("test.dev.json", cfg)
+	suitor.NoError(err)
+	suitor.Equal("test-app", cfg.AppName)
+	suitor.Equal(true, cfg.Debug)
+	suitor.Equal("test", cfg.AppEnv)
+	suitor.Equal("root@tcp(localhost:3306)/test", cfg.Database.DSN)
 }
